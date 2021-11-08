864e72960879e55c2a78b0952c18c93094c588a0
Code Palace <codepalace@fastmail.se>
2021-11-08 14:41:57 +0100

    Added the option to filter currencies

--- Currency App/ContentView.swift
+++ Currency App/ContentView.swift
@@ -14,12 +14,21 @@ struct ContentView: View {
     @State var base = "USD"
     @FocusState private var inputIsFocused: Bool
     
-    func makeRequest() {
+    func makeRequest(showAll: Bool, currencies: [String] = ["USD", "GBP", "EUR"]) {
         apiRequest(url: "https://api.exchangerate.host/latest?base=\(base)&amount=\(input)") { currency in
             //print("ContentView", currency.rates)
             var tempList = [String]()
+            
             for currency in currency.rates {
-                tempList.append("\(currency.key) \(String(format: "%.2f",currency.value))")
+                
+                if showAll {
+                    tempList.append("\(currency.key) \(String(format: "%.2f",currency.value))")
+                } else if currencies.contains(currency.key)  {
+                    tempList.append("\(currency.key) \(String(format: "%.2f",currency.value))")
+                }
+                
+                
+                
                 tempList.sort()
             }
             currencyList.self = tempList
@@ -30,7 +39,6 @@ struct ContentView: View {
     var body: some View {
         VStack() {
             HStack {
-                
                 Text("Currencies")
                     .font(.system(size: 30))
                     .bold()
@@ -38,17 +46,12 @@ struct ContentView: View {
                 Image(systemName: "eurosign.circle.fill")
                     .font(.system(size: 30))
                     .foregroundColor(.blue)
-         
+                
             }
-            
-            
             List {
                 ForEach(currencyList, id: \.self) { currency in
-                    HStack {
-                        Text(currency)
-                          
-                        
-                    }
+                    Text(currency)
+                    
                 }
             }
             
@@ -57,31 +60,31 @@ struct ContentView: View {
                     .frame(height: 8.0)
                     .foregroundColor(.blue)
                     .opacity(0.90)
-
-                    TextField("Enter an amount" ,text: $input)
-                        .padding()
-                        .background(Color.gray.opacity(0.10))
-                        .cornerRadius(20.0)
-                        .padding()
-                        .focused($inputIsFocused)
-    
-        
-                    TextField("Enter a currency" ,text: $base)
-                        .padding()
-                        .background(Color.gray.opacity(0.10))
-                        .cornerRadius(20.0)
-                        .padding()
-                        .focused($inputIsFocused)
-
+                
+                TextField("Enter an amount" ,text: $input)
+                    .padding()
+                    .background(Color.gray.opacity(0.10))
+                    .cornerRadius(20.0)
+                    .padding()
+                    .focused($inputIsFocused)
+                
+                
+                TextField("Enter a currency" ,text: $base)
+                    .padding()
+                    .background(Color.gray.opacity(0.10))
+                    .cornerRadius(20.0)
+                    .padding()
+                    .focused($inputIsFocused)
+                
                 
                 Button("Convert!") {
-                    makeRequest()
+                    makeRequest(showAll: true)
                     inputIsFocused = false
                 }.padding()
             }
             
         }.onAppear() {
-            makeRequest()
+            makeRequest(showAll: true)
         }
     }
 }
